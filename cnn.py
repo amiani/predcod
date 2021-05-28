a2e4163ef8498045378f52f58caa4255c5bf4676
ratang <amianijohns@gmail.com>
2021-05-27 21:00:21 -0400

    add jax

--- cnn.py
+++ cnn.py
@@ -1,5 +1,5 @@
 import numpy as np
-#import jax.numpy as jnp
+from jax import random
 from typing import Tuple, Union, Dict
 
 from fast_layers.fast_layers import conv_forward_fast, conv_backward_fast, max_pool_forward_fast, max_pool_backward_fast
@@ -7,43 +7,44 @@ from fast_layers.fast_layers import conv_forward_fast, conv_backward_fast, max_p
 class CNN:
     def __init__(   self,
                     batch_size: int, C:int, H: int, W: int,
-                    num_filters: int, num_classes: int):
-        self.W1 = np.random.normal(scale=(np.sqrt(2/25)), size=(num_filters, C, 5, 5))
-        self.b1 = np.zeros(num_filters)
+                    num_filters: int, num_classes: int, keys):
+        self.params = {}
+        self.params['W1'] = np.sqrt(2/25) * random.normal(keys[0], (num_filters, C, 5, 5))
+        self.params['b1'] = np.zeros(num_filters)
         num_conv_weights = int(num_filters * (H/2)**2)
-        self.W2 = np.random.normal(scale=(np.sqrt(2/num_conv_weights)), size=(num_conv_weights, num_classes))
-        self.b2 = np.zeros(num_classes)
+        self.params['W2'] = np.sqrt(2/num_conv_weights) * random.normal(keys[1], (num_conv_weights, num_classes))
+        self.params['b2'] = np.zeros(num_classes)
     
-    def forward(self, X: np.ndarray) -> Tuple[np.ndarray, Tuple]:
-        h1, cache1 = conv_relu_forward(X, self.W1, self.b1, self.W1.shape[0])
-        pool_param = { 'pool_height': 2, 'pool_width': 2, 'stride': 2}
-        h2, cache2 = max_pool_forward_fast(h1, pool_param)
-        N, D, H, W = h2.shape
-        scores, cache3 = affine_forward(h2.reshape(N, -1), self.W2, self.b2)
+def forward(params: dict[str, np.ndarray], X: np.ndarray) -> Tuple[np.ndarray, Tuple]:
+    h1, cache1 = conv_relu_forward(X, params['W1'], params['b1'], params['W1'].shape[0])
+    pool_param = { 'pool_height': 2, 'pool_width': 2, 'stride': 2}
+    h2, cache2 = max_pool_forward_fast(h1, pool_param)
+    N, D, H, W = h2.shape
+    scores, cache3 = affine_forward(h2.reshape(N, -1), params['W2'], params['b2'])
 
-        cache = (cache1, cache2, cache3)
-        return scores, cache
+    cache = (cache1, cache2, cache3)
+    return scores, cache
 
-    def loss(self, X: np.ndarray, y: np.ndarray) -> Tuple[np.ndarray, np.ndarray, Tuple]:
-        scores, cache = self.forward(X)
-        loss, dscores = softmax_loss(scores, y)
-        return loss, dscores, cache
+def loss(params: dict[str,np.ndarray], X: np.ndarray, y: np.ndarray) -> Tuple[np.ndarray, np.ndarray, Tuple]:
+    scores, cache = forward(params, X)
+    loss, dscores = softmax_loss(scores, y)
+    return loss, dscores, cache
 
 
-    def backward(self, dscores: np.ndarray, cache: Tuple) -> Dict[str, np.ndarray]:
-        grads = {}
-        conv_relu_cache, pool_cache, affine_cache = cache
-        dh2, dW2, db2 = affine_backward(dscores, affine_cache)
-        grads['W2'] = dW2
-        grads['b2'] = db2
-        #TODO: determine reshape dims more elegantly (cache them?)
-        N, C, H, width = conv_relu_cache[0][0].shape
-        dh1 = max_pool_backward_fast(dh2.reshape(N,25,int(H/2),int(H/2)), pool_cache)
-        dX, dW1, db1 = conv_relu_backward(dh1, conv_relu_cache)
-        grads['W1'] = dW1
-        grads['b1'] = db1
-        #print(grads['W1'])
-        return grads
+def backward( dscores: np.ndarray, cache: Tuple) -> Dict[str, np.ndarray]:
+    grads = {}
+    conv_relu_cache, pool_cache, affine_cache = cache
+    dh2, dW2, db2 = affine_backward(dscores, affine_cache)
+    grads['W2'] = dW2
+    grads['b2'] = db2
+    #TODO: determine reshape dims more elegantly (cache them?)
+    N, C, H, width = conv_relu_cache[0][0].shape
+    dh1 = max_pool_backward_fast(dh2.reshape(N,25,int(H/2),int(H/2)), pool_cache)
+    dX, dW1, db1 = conv_relu_backward(dh1, conv_relu_cache)
+    grads['W1'] = dW1
+    grads['b1'] = db1
+    #print(grads['W1'])
+    return grads
 
 def conv_relu_forward(X: np.ndarray, W: np.ndarray, b: np.ndarray, num_filters: int) -> Tuple[np.ndarray, Tuple]:
     N, C, H, width = X.shape
